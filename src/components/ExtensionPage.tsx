a716872140dcd0742334c45f6cdeccae80156a2d
MrSeager <seager1993@gmail.com>
2025-04-02 09:18:09 +0300

    4/2/2025

---
+++ src/components/ExtensionPage.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+//Components
+import './ExtensionPageStyle.css';
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container } from 'react-bootstrap';
+//Axios
+import axios from 'axios';
+//Spring
+import { useSpring, animated } from '@react-spring/web';
+
+const ExtensionPage: FC = () => {
+    return (
+        <Container fluid>
+            hi
+        </Container>
+    );
+}
+
+export default ExtensionPage;
